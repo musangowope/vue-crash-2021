33fa298da2cd05bdc15b0b0cbae31bf2c832ef95
Musango <musangow@gmail.com>
2021-05-04 15:19:51 +0200

    Tasks.vue: use formatting tool

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,15 +1,17 @@
 <template>
   <div :key="task.id" v-for="task in tasks">
     <Task
-      @delete-task="$emit('delete-task', task.id)"
-      @toggle-reminder="$emit('toggle-reminder', task.id)"
-      :task="task"
+        @delete-task="$emit('delete-task', task.id)"
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        :task="task"
     />
+
   </div>
 </template>
 
 <script>
 import Task from "./Task";
+
 export default {
   name: "Tasks",
   props: {
