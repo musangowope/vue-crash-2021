b223c7299567fe0badd9242c64aa90c71897e0e2
Musango <musangow@gmail.com>
2021-05-04 15:21:17 +0200

    Header.vue: use formatter

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,18 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
-      :text="showAddTask ? 'Close' : 'Add'"
-      :color="showAddTask ? 'red' : 'green'"
-      @btn-click="$emit('toggle-add-task')"
+        :text="showAddTask ? 'Close' : 'Add'"
+        :color="showAddTask ? 'red' : 'green'"
+        @btn-click="$emit('toggle-add-task')"
     />
+
   </header>
+
 </template>
 
 <script>
 import Button from "./Button";
+
 export default {
   name: "Header",
   props: {
